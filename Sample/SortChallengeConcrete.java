029d42ddca8bea04bd9a7e0f1cf393c8b0a6767e
Ramesh <Ramesh@Ramesh-PC>
2020-03-02 12:56:09 -0600

    Initial Version

---
+++ Sample/SortChallengeConcrete.java
@@ -0,0 +1,22 @@
+
+public class SortChallengeConcrete implements SortChallenge {
+
+	@Override
+	public int[] simpleSort(int[] list, boolean ascending) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void printSortedFrequency(int[] list) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int numberOfUniqueValues(int[] list) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

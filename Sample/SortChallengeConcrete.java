4a289f38570b11771e12e987ed39f3f5577ddeec
Ramesh <Ramesh@Ramesh-PC>
2020-03-03 14:39:29 -0600

    Completed Version

--- Sample/SortChallengeConcrete.java
+++ Sample/SortChallengeConcrete.java
@@ -1,22 +1,90 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SortChallengeConcrete implements SortChallenge {
 
 	@Override
 	public int[] simpleSort(int[] list, boolean ascending) {
-		// TODO Auto-generated method stub
-		return null;
+		int[] arr;
+		List<Integer> numbers = new ArrayList<Integer>();
+
+		for (int i : list) {
+			numbers.add(Integer.valueOf(i));
+		}
+		if (ascending) {
+			Collections.sort(numbers);
+		}
+		else {
+			Collections.sort(numbers, Collections.reverseOrder());
+		}	
+		arr = numbers.stream()
+				 .mapToInt(Integer::intValue)
+				 .toArray();
+		return arr;
 	}
 
 	@Override
 	public void printSortedFrequency(int[] list) {
-		// TODO Auto-generated method stub
-		
+		LinkedHashMap<Integer, Integer> lkhmp = new LinkedHashMap<>();
+		List<Integer> numbers = new ArrayList<Integer>();
+
+		for (int i : list) {
+			numbers.add(Integer.valueOf(i));
+		}
+		Collections.sort(numbers);
+
+		// Traverse through array elements and 
+        // count frequencies 
+		for (int i = 0; i < numbers.size(); i++) 
+        { 
+            if (lkhmp.containsKey(numbers.get(i)))  
+            { 
+            	lkhmp.put(numbers.get(i), lkhmp.get(numbers.get(i)) + 1); 
+            }  
+            else
+            { 
+            	lkhmp.put(numbers.get(i), 1); 
+            } 
+        }
+
+		// Traverse through map and print frequencies 
+        for (Map.Entry<Integer, Integer> entry : lkhmp.entrySet()) 
+        { 
+        	int freq = entry.getValue();
+        	String timeStr = "times";
+        	if (freq < 2) {
+        		timeStr = "time";
+        	}
+            System.out.println(entry.getKey() + " appears " + freq + " " + timeStr); 
+        }		
 	}
 
 	@Override
 	public int numberOfUniqueValues(int[] list) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		LinkedHashMap<Integer, Integer> mp = new LinkedHashMap<>();
+		List<Integer> numbers = new ArrayList<Integer>();
 
+		for (int i : list) {
+			numbers.add(Integer.valueOf(i));
+		}
+		Collections.sort(numbers);
+
+		// Traverse through array elements and 
+        // count frequencies 
+		for (int i = 0; i < numbers.size(); i++) 
+        { 
+            if (mp.containsKey(numbers.get(i)))  
+            { 
+                mp.put(numbers.get(i), mp.get(numbers.get(i)) + 1); 
+            }  
+            else
+            { 
+                mp.put(numbers.get(i), 1); 
+            } 
+        }		
+		return mp.entrySet().size();
+	}
 }
---
+++ Sample/SortChallengeMain.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SortChallengeMain {
+	
+	public static int menu;
+	static SortChallenge scc = new SortChallengeConcrete();
+
+	@SuppressWarnings({ "resource" })
+	public static void sortNumbers() {
+
+		Scanner scanner;
+		List<Integer> numbers = new ArrayList<Integer>();
+		System.out.println("Enter the numbers for sorting. Type any key other than number keys and hit <enter> to complete your input.");
+		scanner = new Scanner(System.in);
+        List<Integer> arrList = new ArrayList<Integer>();
+        while (scanner.hasNextInt()) {
+            arrList.add(scanner.nextInt());
+        }
+
+        int[] arr = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+        	arr[i] = arrList.get(i);
+        }
+
+		System.out.println("Type 'desc' and hit <enter> for sorting in descending order. Otherwise the default order is ascending.");
+        scanner = new Scanner(System.in);
+		String asc = scanner.next();
+
+		boolean order = true;
+		if (asc.equals("desc")) {
+			order = false;
+		}
+		
+		int[] arr1 = scc.simpleSort(arr, order);
+		for (int i : arr1) {
+			numbers.add(Integer.valueOf(i));
+		}
+		
+		System.out.println(numbers);
+	}
+	
+	@SuppressWarnings({ "resource" })
+	public static void getSortFrequency() {
+
+		Scanner scanner;
+		System.out.println("Enter numbers. Type any key other than number keys and hit <enter> to complete your input.");
+		scanner = new Scanner(System.in);
+        List<Integer> arrList = new ArrayList<Integer>();
+        while (scanner.hasNextInt()) {
+            arrList.add(scanner.nextInt());
+        }
+
+        int[] arr = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+        	arr[i] = arrList.get(i);
+        }
+		
+		scc.printSortedFrequency(arr);
+	}
+
+	@SuppressWarnings({ "resource" })
+	public static void getNumberOfUniqueValues() {
+
+		Scanner scanner;
+		System.out.println("Enter numbers. Type any key other than number keys and hit <enter> to complete your input.");
+		scanner = new Scanner(System.in);
+        List<Integer> arrList = new ArrayList<Integer>();
+        while (scanner.hasNextInt()) {
+            arrList.add(scanner.nextInt());
+        }
+
+        int[] arr = new int[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
+        	arr[i] = arrList.get(i);
+        }
+
+		int NumOfUniqValues = scc.numberOfUniqueValues(arr);
+		System.out.println("Number of Unique Values is " + NumOfUniqValues);
+	}
+
+	@SuppressWarnings("resource")
+	public static int menu()
+    {
+		System.out.println("**** Number Sorting Functions ****");
+        System.out.println("Choose one option from following : ");
+        System.out.println("1) Simple Sort");
+        System.out.println("2) Print Sorted Frequency");
+        System.out.println("3) Print Number Of Unique Values");
+        System.out.println("4) Exit");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    
+    public static void main(String[] args)
+    {
+        while(menu != 4)
+        {
+            System.out.println();
+            menu = menu();
+            switch (menu)
+            {
+                case 1:
+                    sortNumbers();
+                    break;
+                case 2:
+                	getSortFrequency();
+                    break;
+                case 3:
+                	getNumberOfUniqueValues();
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    }    
+}
